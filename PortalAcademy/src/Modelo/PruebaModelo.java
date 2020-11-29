5e216593870c3cec5f15ce503471a806f577dd22
JuanmapgF <62023322+JuanmapgF@users.noreply.github.com>
2020-11-29 21:01:12 +0100

    MVC

--- PortalAcademy/src/Modelo/PruebaModelo.java
+++ PortalAcademy/src/Modelo/PruebaModelo.java
@@ -3,9 +3,6 @@ package Modelo;
 public class PruebaModelo {
 	
 	public static void main(String[] args) {
-		System.out.println("popo");
-		ayuwoki
-		System.out.println("Juan");
-		System.out.println("Pepe");
+		
 	}
 }
