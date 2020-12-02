59abc9a44d7f9008e633ae50e71409636b340497
pablodt <62577289+pablodt@users.noreply.github.com>
2020-12-02 19:25:22 +0100

    1

--- PortalAcademy/src/Modelo/Usuario.java
+++ PortalAcademy/src/Modelo/Usuario.java
@@ -1,5 +1,29 @@
 package Modelo;
 
 public class Usuario {
-
+	
+	private String nick;
+	private String correo;
+	private String password;
+	
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 }
