ddb238c85a2e820c355628aac9a7ad6e4c0b1fca
JuanmapgF <62023322+JuanmapgF@users.noreply.github.com>
2020-12-02 19:53:45 +0100

    Clase BD y ErrorBD

---
+++ PortalAcademy/src/Modelo/ErrorBD.java
@@ -0,0 +1,14 @@
+package Modelo;
+
+@SuppressWarnings("serial")
+public class ErrorBD extends RuntimeException  
+{
+
+	public ErrorBD() {
+		super();
+	}
+
+	public ErrorBD(String arg0) {
+		super(arg0);
+	}
+}
