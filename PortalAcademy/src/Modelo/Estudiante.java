151cad7bf45067c8967d0e47f902170cc223d4f4
JuanmapgF <62023322+JuanmapgF@users.noreply.github.com>
2020-12-02 20:14:46 +0100

    Actualizacion modelo

--- PortalAcademy/src/Modelo/Estudiante.java
+++ PortalAcademy/src/Modelo/Estudiante.java
@@ -1,5 +1,22 @@
 package Modelo;
 
+import java.util.List;
+
 public class Estudiante extends Usuario {
+	
+	private List<Curso> cursos;
+	private List<Actividad> actividades;
+	public List<Curso> getCursos() {
+		return cursos;
+	}
+	public void setCursos(List<Curso> cursos) {
+		this.cursos = cursos;
+	}
+	public List<Actividad> getActividades() {
+		return actividades;
+	}
+	public void setActividades(List<Actividad> actividades) {
+		this.actividades = actividades;
+	}
 
 }
