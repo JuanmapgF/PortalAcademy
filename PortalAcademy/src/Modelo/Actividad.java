1fd2bd58a9a4bf559ac89a32c8bcda6ef62a75db
pablodt <62577289+pablodt@users.noreply.github.com>
2020-12-02 19:38:12 +0100

    Actividad

--- PortalAcademy/src/Modelo/Actividad.java
+++ PortalAcademy/src/Modelo/Actividad.java
@@ -10,4 +10,62 @@ public class Actividad {
 	private int aforo;
 	private Date fecha;
 	private String lugar;
+	
+	private Usuario participante;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+
+	public int getAforo() {
+		return aforo;
+	}
+
+	public void setAforo(int aforo) {
+		this.aforo = aforo;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getLugar() {
+		return lugar;
+	}
+
+	public void setLugar(String lugar) {
+		this.lugar = lugar;
+	}
+
+	public Usuario getParticipante() {
+		return participante;
+	}
+
+	public void setParticipante(Usuario participante) {
+		this.participante = participante;
+	}
 }
