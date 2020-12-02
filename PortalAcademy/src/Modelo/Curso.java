dc2f2c879d925643e3fbfcf39433ad2580e2ca40
davidlnk <lenke@uma.es>
2020-12-02 20:03:10 +0100

    Update Curso.java

--- PortalAcademy/src/Modelo/Curso.java
+++ PortalAcademy/src/Modelo/Curso.java
@@ -2,13 +2,16 @@ package Modelo;
 
 public class Curso {
 	
-	private Integer id;
+	private Integer idCurso;
 	private String nombre;
 	private String descripcion;
 	private String imagen;
 	private Boolean publico;
 	private Integer aforo;
 	private Boolean presencial;
+	
+	private String server;
+	private String databaseName;
 	
 	public Curso(String nombre, String descripcion, String imagen, Boolean publico, Integer aforo, Boolean presencial) {
 		this.nombre = nombre;
@@ -19,7 +22,14 @@ public class Curso {
 		this.presencial = presencial;
 	}
 
-	public Curso(Integer id) {
-		
+	public Curso(Integer idCurso) {
+		BD miBD = new BD(server, databaseName);
+		List<Object[]> datos = miBD.Select("SELECT * FROM Curso WHERE idCurso = " + idCurso);
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.imagen = imagen;
+		this.publico = publico;
+		this.aforo = aforo;
+		this.presencial = presencial;
 	}
 }
