34faebadc05d884e5ab8366f5d30fbe14c306f94
IvanHijanoPerez <62576321+IvanHijanoPerez@users.noreply.github.com>
2020-12-06 17:46:09 +0100

    Organizacion, Profesor, Usuario, Estudiante

--- PortalAcademy/src/Modelo/Profesor.java
+++ PortalAcademy/src/Modelo/Profesor.java
@@ -1,30 +1,50 @@
 package Modelo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Profesor extends Usuario {
 	
-	private String telefono;
+	private int telefono;
 	private List<Curso> cursos;
-
+	
+	public Profesor(String nick, String correo, String password, int telefono) {
+		super(nick,correo,password);
+		this.telefono = telefono;
+		this.cursos = new ArrayList<Curso>();
+	}
+	
 	public Profesor(String idProfesor) {
+		super(idProfesor);
 		
+		//ACCESO BASEDATOS PARA OBTENER INFO PROFESOR
+		int telefono;	
+		this.telefono = telefono;
+		
+		List<Curso> c = new ArrayList<Curso>();
+		//ACCESO BASEDATOS PARA OBTENER CURSOS DEL PROFESOR
+		Curso curso;
+		c.add(curso);
+		for(Curso cu : c) {
+			cursos.add(cu);
+		}
 	}
 
 	public List<Curso> getCursos() {
-		return cursos;
+		return this.cursos;
 	}
 
-	public void setCursos(List<Curso> cursos) {
-		this.cursos = cursos;
-	}
-
-	public String getTelefono() {
-		return telefono;
-	}
-
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
+	public void anadirCurso(Curso curso) {
+		cursos.add(curso);
+		//ACCESO BASEDATOS PARA AÑADIR CURSO AL PROFESOR
 	}
 	
+	public void eliminarCurso(Curso curso) {
+		cursos.remove(curso);
+		//ACCESO BASEDATOS PARA ELIMINAR CURSO DEL PROFESOR
+	}
+
+	public int getTelefono() {
+		return this.telefono;
+	}
 }
