a3d6399fc8c11c9b1582dcf982e3a8d271bfdd89
pablodt <62577289+pablodt@users.noreply.github.com>
2020-12-07 19:30:51 +0100

    1

---
+++ PortalAcademy/src/Controlador/ControladorInicio.java
@@ -0,0 +1,23 @@
+package Controlador;
+
+import java.awt.event.*;
+
+import Vista.*;
+
+public class ControladorInicio implements ActionListener {
+	
+	private Inicio panel;
+	
+	public ControladorInicio(Inicio p) {
+		panel = p;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getActionCommand().equals("SELECCION")) {
+			
+		}
+	}
+
+}
