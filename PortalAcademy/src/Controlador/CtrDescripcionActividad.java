d61b994e2ddb9cbf88dc6938671487496551c12b
davidlnk <lenke@uma.es>
2020-12-11 21:46:36 +0100

    DescripcionActividad

---
+++ PortalAcademy/src/Controlador/CtrDescripcionActividad.java
@@ -0,0 +1,53 @@
+package Controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.ParseException;
+
+import Modelo.*;
+import Vista.*;
+
+public class CtrDescripcionActividad implements ActionListener {
+	private Usuario user;
+	private Actividad actividad;
+	
+	private DescripcionActividad ventana;
+	
+	public CtrDescripcionActividad(Usuario user, Actividad actividad) {
+		this.user = user;
+		this.actividad = actividad;
+		Boolean esInvitado = user == null;
+		Boolean esEstudiante = user != null && user instanceof Estudiante;
+		Boolean quedaEspacio = actividad.getAforo() < actividad.getParticipantes().size();
+		Boolean estaEnActividad;
+		if (esEstudiante) {
+			estaEnActividad = ((Estudiante) user).getActividades().contains(actividad);
+		} else {
+			estaEnActividad = false;
+		}
+		Boolean usuarioPuedeUnirse = (esInvitado || esEstudiante) && quedaEspacio && !estaEnActividad;
+		ventana = new DescripcionActividad(actividad.getNombre(), actividad.getDescripcion(), usuarioPuedeUnirse);
+		ventana.controlador(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getActionCommand().equals("Unirse")) {
+			actividad.addParticipante(user);
+			//TODO: Entramos en informacionActividad
+		}
+		
+		if (e.getActionCommand().equals("Cerrar Sesi\u00F3n")) {
+			try {
+				CtrExplorar c = new CtrExplorar(new Explorar(Curso.getTodosLosCursos(), Actividad.getTodasLasActividades()), new Menu(null));
+				Main.setPanel(c.getPanel());
+			} catch (ParseException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+		}
+	}
+
+}
---
+++ PortalAcademy/src/Vista/DescripcionActividad.java
@@ -0,0 +1,49 @@
+package Vista;
+
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+@SuppressWarnings("serial")
+public class DescripcionActividad extends JPanel {
+	private JButton unirse;
+	private JButton cerrarSesion;
+
+	/**
+	 * Create the panel.
+	 */
+	public DescripcionActividad(String nombre, String descripcion, Boolean puedeUnirse) {
+		this.setBounds(0, 0, 1080, 650);
+		setLayout(null);
+		
+		JLabel nombreActividad = new JLabel(nombre);
+		nombreActividad.setBounds(428, 34, 269, 33);
+		add(nombreActividad);
+		
+		JTextArea descripcionActividad = new JTextArea(descripcion);
+		descripcionActividad.setBounds(278, 102, 652, 371);
+		add(descripcionActividad);
+		
+		if (puedeUnirse) {
+			unirse = new JButton("Unirse");
+			unirse.setBounds(278, 511, 89, 23);
+			add(unirse);
+		}
+		
+		cerrarSesion = new JButton("Cerrar Sesi\u00F3n");
+		cerrarSesion.setBounds(952, 39, 118, 23);
+		add(cerrarSesion);
+	}
+	
+	public void controlador(ActionListener ctr) {
+		unirse.addActionListener(ctr);
+		unirse.setActionCommand("Unirse");
+		
+		cerrarSesion.addActionListener(ctr);
+		cerrarSesion.setActionCommand("Cerrar Sesi\u00F3n");
+	}
+
+}
