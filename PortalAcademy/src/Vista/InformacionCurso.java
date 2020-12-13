c89749218651f74b76e9aa6e0e3376be5809ccb3
davidlnk <lenke@uma.es>
2020-12-13 23:35:41 +0100

    Update InformacionCurso.java

--- PortalAcademy/src/Vista/InformacionCurso.java
+++ PortalAcademy/src/Vista/InformacionCurso.java
@@ -1,10 +1,14 @@
 package Vista;
 
+import java.awt.event.ActionListener;
 import java.util.List;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class InformacionCurso extends JPanel {
@@ -13,9 +17,14 @@ public class InformacionCurso extends JPanel {
 	/**
 	 * Create the panel.
 	 */
-	public InformacionCurso(String nombre, String descripcion, List<String> mensajes) {
+	public InformacionCurso(String nombre, String descripcion, Boolean tieneForo, List<String> mensajes) {
 		this.setBounds(0, 0, 1080, 650);
 		setLayout(null);
+		
+//		if (tieneForo) {
+			//TODO: Foro
+//		}
+		
 		
 		JLabel nombreCurso = new JLabel(nombre);
 		nombreCurso.setBounds(428, 34, 269, 33);
@@ -28,6 +37,19 @@ public class InformacionCurso extends JPanel {
 		cerrarSesion = new JButton("Cerrar Sesi\u00F3n");
 		cerrarSesion.setBounds(952, 39, 118, 23);
 		add(cerrarSesion);
+		
+		
+	}
+	
+	public InformacionCurso(String nombre, String descripcion, Boolean publico, Integer aforo,
+			Boolean presencial, Boolean tieneForo) {
+		
+		
+	}
+	
+	public void controlador(ActionListener ctr) {		
+		cerrarSesion.addActionListener(ctr);
+		cerrarSesion.setActionCommand("Cerrar Sesi\u00F3n");
 	}
 
 }
