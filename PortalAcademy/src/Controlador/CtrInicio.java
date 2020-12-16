670573ec3e4355c5622caf62a2e108925889e7fc
pablodt <62577289+pablodt@users.noreply.github.com>
2020-12-16 18:31:15 +0100

    1

--- PortalAcademy/src/Controlador/CtrInicio.java
+++ PortalAcademy/src/Controlador/CtrInicio.java
@@ -4,6 +4,7 @@ import java.awt.event.*;
 import java.text.ParseException;
 import java.util.List;
 
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import Vista.*;
@@ -85,10 +86,10 @@ public class CtrInicio implements ActionListener {
 					CtrExplorar ex = new CtrExplorar(new Explorar(est, Curso.getTodosLosCursos()));
 					Main.setPanel(ex.getPanel());
 				} else {
-					ventana.mostrarError();
+					JOptionPane.showConfirmDialog(ventana, "El usuario o contraseña introducidos son erróneos");
 				}
 			} else {
-				ventana.mostrarError();
+				JOptionPane.showConfirmDialog(ventana, "El usuario o contraseña introducidos son erróneos");
 			}
 
 		}
