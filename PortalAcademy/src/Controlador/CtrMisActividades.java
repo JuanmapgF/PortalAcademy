51af5d676a0bd75dfdd928d0a2bfcaf6c6f4ab30
IvanHijanoPerez <62576321+IvanHijanoPerez@users.noreply.github.com>
2020-12-16 01:01:05 +0100

    Cambio 20

--- PortalAcademy/src/Controlador/CtrMisActividades.java
+++ PortalAcademy/src/Controlador/CtrMisActividades.java
@@ -44,16 +44,20 @@ public class CtrMisActividades implements ActionListener {
 		if (e.getActionCommand().equals("MISACTIVIDADESUSUARIO")) {
 			Actividad c = ventana.getA();
 			if(c!=null) {
-				//CtrRegistro cr = new CtrRegistro(new Registro());
-				//Main.setPanel(cr.getPanel());
+				if(ventana.esEstudiante()) {
+					CtrInformacionActividad cr = new CtrInformacionActividad(ventana.getEstudiante(),c);
+					Main.setPanel(cr.getPanel());
+				}
 			}	
 		}
 		
 		if (e.getActionCommand().equals("MISACTIVIDADESORGANIZACION")) {
 			Actividad c = ventana.getA();
 			if(c!=null) {
-				//CtrRegistro cr = new CtrRegistro(new Registro());
-				//Main.setPanel(cr.getPanel());
+				if(ventana.esOrganizacion()) {
+					CtrInformacionActividad cr = new CtrInformacionActividad(ventana.getOrganizacion(),c);
+					Main.setPanel(cr.getPanel());
+				}
 			}
 		}
 		
