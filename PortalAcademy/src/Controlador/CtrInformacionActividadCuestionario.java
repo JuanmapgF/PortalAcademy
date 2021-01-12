044793afb0b1e7f67920ae9f739779263ed3876b
KikoBM <kikobm7@uma.es>
2021-01-12 21:50:19 +0100

    cuestionario actividad

--- PortalAcademy/src/Controlador/CtrInformacionActividadCuestionario.java
+++ PortalAcademy/src/Controlador/CtrInformacionActividadCuestionario.java
@@ -10,14 +10,18 @@ import java.net.URISyntaxException;
 import javax.swing.JPanel;
 
 import Modelo.Actividad;
+import Modelo.Curso;
 import Modelo.Usuario;
 import Vista.InformacionActividadCuestionario;
+import Vista.Main;
 
 public class CtrInformacionActividadCuestionario implements ActionListener {
 
 	private InformacionActividadCuestionario vista;
 	private Actividad actividad;
-	private String enalceCuestionario = "https://forms.gle/A6w9nd54iKBxBfFTA";
+	private String enlaceCuestionario = "https://forms.gle/A6w9nd54iKBxBfFTA";
+	
+	private Usuario user;
 
 	public CtrInformacionActividadCuestionario(Actividad actividad, Usuario user) {
 		vista = new InformacionActividadCuestionario(user, actividad);
@@ -32,26 +36,44 @@ public class CtrInformacionActividadCuestionario implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		if (e.getActionCommand().equals("Crear")) {
 			actividad.setSatisfaccion(true);
+			CtrInformacionActividadCuestionario c = new CtrInformacionActividadCuestionario(actividad,user);
+			Main.setPanel(c.getPanel());
 		}
 
 		if (e.getActionCommand().equals("Borrar")) {
 			actividad.setSatisfaccion(false);
+			CtrInformacionActividadCuestionario c = new CtrInformacionActividadCuestionario(actividad,user);
+			Main.setPanel(c.getPanel());
 		}
 
 		if (e.getActionCommand().equals("Hacer")) {
-			enlace(enalceCuestionario);
+			try {
+				Abrir_URL(enlaceCuestionario);
+			} catch (URISyntaxException e1) {
+				e1.printStackTrace();
+			}finally {
+				CtrInformacionActividadCuestionario c = new CtrInformacionActividadCuestionario(actividad,user);
+				Main.setPanel(c.getPanel());
+			}
 		}
 		// TODO Auto-generated method stub
 
 	}
 
-	public void enlace(String enlaceAAceder) {
-		Desktop enlace = Desktop.getDesktop();
-		try {
-			enlace.browse(new URI(enlaceAAceder));
-		} catch (IOException | URISyntaxException e) {
-			e.getMessage();
-		}
+	
+	
+	private void Abrir_URL(String url) throws URISyntaxException {
+		 if (Desktop.isDesktopSupported()) {
+			 Desktop desktop = Desktop.getDesktop();
+			 try {
+				 URI uri = new URI(url);
+				 desktop.browse(uri);
+			 } catch (IOException e) {
+				 System.err.println("Error: No se pudo abrir el enlace" + e.getMessage() );
+			 }
+		 } else {
+			 System.err.println("Error: No se puede abrir enlaces web.");
+		 }
 	}
 
 }
